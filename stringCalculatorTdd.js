d99e7e9af63c331bd33df3ce79f0005d273ee93f
Syed Adnan Ali <syed.adnan@acronotics.com>
2025-09-17 08:09:41 +0530

    Custom delimiter parsing implemented

--- stringCalculatorTdd.js
+++ stringCalculatorTdd.js
@@ -1,13 +1,34 @@
+
+function escapeForRegex(s) {
+    return s.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+}
+
 function add(numbers) {
     if (numbers === "") return 0;
 
-    if (!numbers.includes(',')) {
-        const number = Number(numbers);
+    let input = numbers;
+
+    if (!input.includes(',')) {
+        const number = Number(input);
         if (!Number.isNaN(number)) return number;
     }
 
-    const numbersSum = numbers.split(/[\n,]/).map(Number)
-    return numbersSum.reduce((acc, curr) => acc + curr, 0);
+    let splitRegex = /,|\n/;
+
+    if (input.startsWith("//")) {
+        const delimiterEndIndex = input.indexOf("\n");
+        const customDelimiter = input.substring(2, delimiterEndIndex);
+        input = input.slice(delimiterEndIndex + 1);
+
+        splitRegex = new RegExp(escapeForRegex(customDelimiter));
+    }
+
+    const filteredNumbers = input
+        .split(splitRegex)
+        .filter(number => number !== "" && number !== undefined)
+        .map(number => Number(number));
+
+    return filteredNumbers.reduce((acc, curr) => acc + curr, 0);
 
 }
 
