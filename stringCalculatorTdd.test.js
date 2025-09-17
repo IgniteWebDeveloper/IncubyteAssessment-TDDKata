78da6c3cd3ac0b9c91feed9cd68124b322c9b683
Syed Adnan Ali <syed.adnan@acronotics.com>
2025-09-17 07:36:01 +0530

    add method can handle handle new lines between numbers instead of commas

--- stringCalculatorTdd.test.js
+++ stringCalculatorTdd.test.js
@@ -9,3 +9,7 @@ test("returns sum of the numbers in the string", () => {
     expect(add("1,2,3,4,5")).toBe(15);
 });
 
+test("handle new lines between numbers", () => {
+    expect(add("1\n2,3")).toBe(6);
+});
+
