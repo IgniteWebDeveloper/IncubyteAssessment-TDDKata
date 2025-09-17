7fbea0250d3b2b9bf51adf43ad31497dc41822db
Syed Adnan Ali <syed.adnan@acronotics.com>
2025-09-17 07:31:39 +0530

    add method can handle any amount of numbers

--- stringCalculatorTdd.js
+++ stringCalculatorTdd.js
@@ -1,6 +1,14 @@
 function stringCalculatorTdd(numbers) {
     if (numbers === "") return 0;
-    throw new Error("Not implemented yet");
+
+    if (!numbers.includes(',')) {
+        const number = Number(numbers);
+        if (!Number.isNaN(number)) return number;
+    }
+
+    const numbersSum = numbers.split(",").map(Number)
+    return numbersSum.reduce((acc, curr) => acc + curr, 0);
+
 }
 
 module.exports = { stringCalculatorTdd };
--- stringCalculatorTdd.test.js
+++ stringCalculatorTdd.test.js
@@ -3,3 +3,9 @@ const { stringCalculatorTdd } = require("./stringCalculatorTdd");
 test("returns 0 for empty string", () => {
     expect(stringCalculatorTdd("")).toBe(0);
 });
+
+
+test("returns sum of the numbers in the string", () => {
+    expect(stringCalculatorTdd("1,5")).toBe(6);
+});
+
